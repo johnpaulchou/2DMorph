4cd89681c4998e1f9ce43c887eadff4ed56efb48
John Paul Chou <john.paul.chou@cern.ch>
2024-04-05 17:16:15 -0400

    skelton

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <RooRealVar.h>
+#include <RooDataHist.h>
+#include <RooGaussian.h>
+#include <RooPlot.h>
+
+using namespace RooFit;
+using namespace std;
+int main()
+{
+  cout << "hello world" << endl;
+  return 0;
+}
